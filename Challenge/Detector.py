159e083f1b8c110ab982b9f236b2c007782f2422
Moises Lopez <mdlopezme@gmail.com>
2022-03-15 15:06:48 -0700

    simplified code

--- Challenge/Detector.py
+++ Challenge/Detector.py
@@ -13,6 +13,10 @@ class DetectCircle(sql):
         self.cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
         self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
 
+        # Good to initiliaze
+        self.pv = 0.0
+        self.detected = False
+
         # Check if the webcam is opened correctly
         if not self.cap.isOpened():
             raise IOError("Cannot open webcam") 
@@ -25,8 +29,7 @@ class DetectCircle(sql):
 
         # convert to hsv colorspace
         hsv = cv.cvtColor(self.frame, cv.COLOR_BGR2HSV)
-        self.mask = cv.inRange(hsv, np.array([self.lower_hue1,self.lower_sat1,self.lower_val1]) , 
-            np.array([self.upper_hue1, self.upper_sat1, self.upper_val1]))
+        self.mask = cv.inRange(hsv, np.array(self.lower_hsv) , np.array(self.upper_hsv))
 
         # Opening operation on red_only for denoising
         self.mask=cv.morphologyEx(self.mask, cv.MORPH_OPEN, np.ones((5,5),np.uint8))
@@ -54,19 +57,26 @@ class DetectCircle(sql):
         if int(M["m00"]) != 0:
             # calculate x,y coordinate of center
             cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
 
-            h, w = np.shape(self.mask)
-            relX = (cX - w/2)/w
-            relY = (h-cY)/h
+            # Convert to relative scale of (-1,1)
+            _, w = np.shape(self.mask)
+            self.pv = 2*(cX - w/2)/w
 
-            print((int(relX*100),int(relY*100)))
+            # Object was detected
+            self.detected = True
+        else:
+            self.detected = False
+
 
 def main():
     detector = DetectCircle()
     
     while True:
         detector.hsv_search()
+        if(detector.detected):
+            print(f'Process var: {detector.pv}')
+        else:
+            print('Not Found!')
         # Wait for q keypress or KeyboardInterrupt event to occur
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
--- Challenge/SQLManager.py
+++ Challenge/SQLManager.py
@@ -2,9 +2,5 @@ class SQLManager():
     def __init__(self):
         # I'm setting some constants for now so I can work with the object detectionq
         # Set Color detection paramenters
-        self.lower_hue1 = 0
-        self.lower_sat1 = 144
-        self.lower_val1 = 57
-        self.upper_hue1 = 23
-        self.upper_sat1 = 255
-        self.upper_val1 = 185
+        self.lower_hsv = [0, 144, 57]
+        self.upper_hsv = [23,255,185]
