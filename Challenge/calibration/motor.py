fe8a5a2f0efb2adc1ee535725c5bc8095dffa39c
Moises Lopez <mdlopezme@gmail.com>
2022-03-15 16:00:27 -0700

    use SteperMotor class as proxy for BYJMotors

--- Challenge/calibration/motor.py
+++ Challenge/calibration/motor.py
@@ -1,27 +1,36 @@
 import RPi.GPIO as GPIO
 import cv2 as cv
-
 # import the library
 from RpiMotorLib import RpiMotorLib
 
-GpioPins = [18, 23, 24, 25]
+class StepperMotor(RpiMotorLib.BYJMotor):
+    def __init__(self,GpioPins = [18, 23, 24, 25]) -> None:
+        super().__init__("MyMotorOne", "28BYJ")
+        self.pins = GpioPins
+        # Declare a named instance of class pass a name and motor type
+        self.motor = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
 
-# Declare a named instance of class pass a name and motor type
-mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
+    def create_controls(self):
+        # call the function pass the parameters
+        cv.namedWindow('Controls')
+        # Create trackbars
+        cv.createTrackbar('Steps', 'Controls', 0, 255, self.update_steps)
+        cv.createTrackbar('Wait', 'Controls', 0, 255, self.update_wait)
 
+    def run(self, wait, steps, ccwise):
+        super().motor_run(self.pins,wait,steps,ccwise,False,"full",.05)
 
-# call the function pass the parameters
-cv.namedWindow('Controls')
-# Create trackbars
-cv.createTrackbar('Steps', 'Controls', 0, 255, update_steps)
-cv.createTrackbar('Wait', 'Controls', 0, 255, update_wait)
+    def __del__(self):
+        GPIO.cleanup()
 
-#send 5 step signals 50 times in each direction.
-for i in range(50):
-    #           (pins, wait, steps, ccwise, verbose, steptype, initdelay)
-    mymotortest.motor_run(GpioPins , .002, 5, False, False, "full", .05)
-for i in range(50):
-    mymotortest.motor_run(GpioPins , .002, 5, True, False, "full", .05)
+def main():
+    motor = StepperMotor()
+    #send 5 step signals 50 times in each direction.
+    for i in range(50):
+        #           (pins, wait, steps, ccwise, verbose, steptype, initdelay)
+        motor.run(0.002,5,False)
+    for i in range(50):
+        motor.run(0.002,5,True)
 
-# good practise to cleanup GPIO at some point before exit
-GPIO.cleanup()
+if __name__ == '__main__':
+    main()
