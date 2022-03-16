aa0e16f2111b2cc7df344895f3da65d6f4d88588
Moises Lopez <mdlopezme@gmail.com>
2022-03-15 18:53:22 -0700

    queue for MotorController

--- Challenge/MotorController.py
+++ Challenge/MotorController.py
@@ -1,4 +1,9 @@
 from RpiMotorLib import RpiMotorLib
+import cv2 as cv
+from SQLManager import SQLManager
+from Detector import Detector
+from queue import Queue
+from threading import Thread
 
 class StepperMotor(RpiMotorLib.BYJMotor):
     def __init__(self,GpioPins = [18, 23, 24, 25]) -> None:
@@ -20,15 +25,46 @@ class StepperMotor(RpiMotorLib.BYJMotor):
             # Calculate wait
             wait = 0.02 - abs(speed)*(0.02 - 0.002)
             super().motor_run(self.pins,wait,1,ccwise,False,"half",.0)
+    
+    def start(self,in_q):
+        while True:
+            # Get some data
+            [detected,pv] = in_q.get()
+            if(detected):
+                print(f'Process var: {pv}')
+                self.run(pv)
+            else:
+                self.run()
+                print('Not Found!')
 
 def main():
     motor = StepperMotor()
+    sql = SQLManager()
+    detector = Detector(sql.lower_hsv,sql.upper_hsv)
+    q = Queue()
 
-    while True:
-        for i in range(-100,100):
-            motor.run(i/100.0)
+    thread1 = Thread(target=detector.start, args=(q,))
+    thread2 = Thread(target = motor.start, args =(q, ))
 
-    # motor.__del__()
+    thread1.start()
+    thread2.start()
+
+    # try:
+    #     while True:
+    #         detector.update()
+            
+    #         if(detector.detected):
+    #             print(f'Process var: {detector.pv}')
+    #             motor.run(detector.pv)
+    #         else:
+    #             motor.run()
+    #             print('Not Found!')
+                
+    #         cv.imshow('Webcam', detector.frame)
+    #         if cv.waitKey(1) & 0xFF == ord('q'):
+    #             break
+    # except KeyboardInterrupt:
+    #     motor.run()
 
 if __name__ == '__main__':
     main()
