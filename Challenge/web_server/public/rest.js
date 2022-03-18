25b7138cc340b15952cd5d5d930a42c60aa7b7b8
Olivier <orogers@ucsd.edu>
2022-03-18 16:31:55 -0700

    comments

--- Challenge/web_server/public/rest.js
+++ Challenge/web_server/public/rest.js
@@ -1,3 +1,5 @@
+// Populate select menu on page load, hide stuff, and
+// load a new image.
 document.addEventListener("DOMContentLoaded", function() {
   console.log('Document Loaded')
   let object_select=document.getElementById('object')
@@ -16,6 +18,8 @@ document.addEventListener("DOMContentLoaded", function() {
   updatePage();
 });
 
+// Send object search request and start updating the
+// page.
 function detectObject() {
   try {
     clearInterval(intervalID);
@@ -26,6 +30,7 @@ function detectObject() {
   intervalID = setInterval(updatePage,500);
 }
 
+// Update the page based on the system's state. 
 function updatePage() {
   updateImage();
   let the_object=document.getElementById('object').value;
@@ -42,6 +47,8 @@ function updatePage() {
     return
   }
 
+  // The object_found route returns whether or not
+  // the system has found an object.
   fetch('/object_found')
     .then(response=>response.json())
     .then(function(response){
@@ -67,6 +74,7 @@ function updateImage() {
   // append time to force fetch instead of using cache
 }
 
+// Reverse geocode and raw
 function getCoords() {
   fetch('/get_coords')
     .then(response=>response.json())
@@ -81,12 +89,14 @@ function getCoords() {
     })
 }
 
+// Tell the server to add a MySQL entry
 function save_object() {
   hide_section('save_object');
   let the_object=document.getElementById('object').value;
   fetch('/save_object?object='+the_object);
 }
 
+// Repopulate the table row
 function inject_response(response,tableID) {
   let theTable=document.getElementById(tableID);
   let rowCount = theTable.rows.length;
